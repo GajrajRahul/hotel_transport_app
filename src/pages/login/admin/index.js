ca1ea12a8f9bb2eabb76e99f23739a9f4a79e04f
Vikas Sharma <vikassharma@Vikass-MacBook-Air.local>
2025-01-28 20:46:32 +0530

    Changes in Login, Faqs Page created for login

--- src/pages/login/admin/index.js
+++ src/pages/login/admin/index.js
@@ -8,15 +8,12 @@ import toast from 'react-hot-toast'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import Typography from '@mui/material/Typography'
 import FormControl from '@mui/material/FormControl'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import FormHelperText from '@mui/material/FormHelperText'
 import InputAdornment from '@mui/material/InputAdornment'
 import IconButton from '@mui/material/IconButton'
-import Button from '@mui/material/Button'
 
 import { useTheme } from '@mui/material/styles'
 import { styled } from '@mui/material/styles'
@@ -25,7 +22,10 @@ import BlankLayout from 'src/@core/layouts/BlankLayout'
 import { useAuth } from 'src/hooks/useAuth'
 
 import Icon from 'src/@core/components/icon'
-import { DarkHelpIcon } from 'src/utils/icons'
+import { DarkHelpIcon, FAQsIcon, HandShake } from 'src/utils/icons'
+import { CardContent, Typography, Tooltip, Popover, Button } from '@mui/material'
+
+import { useRouter } from 'next/router'
 
 const BoxWrapper = styled(Box)(({ theme }) => ({
   [theme.breakpoints.down('xl')]: {
@@ -59,18 +59,57 @@ const AdminLoginPage = () => {
   const [showPassword, setShowPassword] = useState(false)
   const theme = useTheme()
   const hidden = useMediaQuery(theme.breakpoints.down('md'))
-
+  const router = useRouter()
   const [currentImage, setCurrentImage] = useState(0)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [open, setOpen] = useState(false)
+  const [background, setBackground] = useState('')
 
   const auth = useAuth()
 
+  const getRandomBackground = () => {
+    const randomIndex = Math.floor(Math.random() * urls.length)
+    return urls[randomIndex]
+  }
+
   useEffect(() => {
+    // Set the initial random background
+    setBackground(getRandomBackground())
+
+    // Interval to change the background image (or other behavior) every 5 seconds
     const interval = setInterval(() => {
       setCurrentImage(prevImage => (prevImage + 1) % images.length)
-    }, 5000) // 5000ms = 5 seconds
+    }, 5000)
 
-    return () => clearInterval(interval) // Clean up on unmount
-  }, [])
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval)
+  }, []) // Empty dependency array ensures this runs once on mount
+
+  let urls = [
+    '/images/bakcground-Images/background-arh-indiagate.jpeg',
+    '/images/bakcground-Images/background-arh-tajmaha.jpeg',
+    '/images/bakcground-Images/background-arh-dubai.jpeg',
+    '/images/bakcground-Images/background-arh-dubaiburjkhalifa.jpeg',
+    '/images/bakcground-Images/background-arh-dubaiskyview.jpeg',
+    '/images/bakcground-Images/background-arh-hawamahal.jpeg',
+    '/images/bakcground-Images/background-arh-himachalforest.jpeg',
+    '/images/bakcground-Images/background-arh-himachalmountains.jpeg'
+  ]
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+    setOpen(!open) // Toggle the popover visibility
+  }
+
+  // Handle the close of the Popover
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleFaqClick = () => {
+    // Redirect to the FAQ page
+    router.push('/faqs') // Make sure to replace '/faq' with your actual FAQ route
+  }
 
   const {
     control,
@@ -97,10 +136,12 @@ const AdminLoginPage = () => {
     <Box className='content-right'>
       <Box
         sx={{
+          backgroundImage: `url(${background})`,
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'space-between',
           width: '100%',
+          height: '100vh',
           px: 10
         }}
       >
@@ -111,10 +152,6 @@ const AdminLoginPage = () => {
           container
           spacing={6}
           alignItems={'center'}
-
-          // sx={{
-          //   border: '1px solid red'
-          // }}
         >
           {!hidden && (
             <Grid item xs={12} md={6}>
@@ -133,7 +170,7 @@ const AdminLoginPage = () => {
             <Card className='inner-card'>
               <Box
                 sx={{
-                  p: '0% 5% 5% 5%',
+                  p: '3% 5% 7% 5%',
                   height: '100%',
                   display: 'flex',
                   alignItems: 'center',
@@ -145,13 +182,13 @@ const AdminLoginPage = () => {
                   <Box sx={{ mb: 6, mt: 5 }}>
                     <TypographyStyled
                       sx={{ typography: { xs: 'h4', laptopSm: 'h3' }, fontWeight: '700 !important' }}
-                      color='#333333'
+                      color='#ffffff'
                     >
                       Welcome Back!
                     </TypographyStyled>
                     <Typography
                       sx={{ typography: { xs: 'body2', laptopSm: 'body1' }, fontWeight: '500 !important' }}
-                      color='#333333'
+                      color='#ffffff'
                     >
                       Let's Plan Journey's Together!
                     </Typography>
@@ -236,7 +273,7 @@ const AdminLoginPage = () => {
                         textAlign: 'end'
                       }}
                     >
-                      <LinkStyled sx={{ color: '#333333' }} href='/forgot-password'>
+                      <LinkStyled sx={{ color: '#ffffff', fontWeight: '500' }} href='/forgot-password'>
                         Recover Password?
                       </LinkStyled>
                     </Typography>
@@ -253,14 +290,30 @@ const AdminLoginPage = () => {
                     </Button>
                     <Box sx={{ mt: 5, display: 'flex', flexDirection: 'column' }}>
                       <Typography
-                        sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '0.75rem' } }}
+                        sx={{
+                          fontSize: {
+                            xs: '0.56rem',
+                            sm: '0.75rem',
+                            md: '0.56rem',
+                            laptopSm: '0.75rem',
+                            fontWeight: '500'
+                          }
+                        }}
                         variant='caption'
-                        color='#333333'
+                        color='#ffffff'
                       >
                         By logging in, you accept the adventurerichaholidays.com
                       </Typography>
                       <Typography
-                        sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '0.75rem' } }}
+                        sx={{
+                          fontSize: {
+                            xs: '0.56rem',
+                            sm: '0.75rem',
+                            md: '0.56rem',
+                            laptopSm: '0.75rem',
+                            fontWeight: '500'
+                          }
+                        }}
                         variant='caption'
                         color='white'
                       >
@@ -268,9 +321,17 @@ const AdminLoginPage = () => {
                           Terms & conditions
                         </CustomLinkStyled>
                         <Typography
-                          sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '0.75rem' } }}
+                          sx={{
+                            fontSize: {
+                              xs: '0.56rem',
+                              sm: '0.75rem',
+                              md: '0.56rem',
+                              laptopSm: '0.75rem',
+                              fontWeight: '500'
+                            }
+                          }}
                           variant='caption'
-                          color='#333333'
+                          color='#ffffff'
                           marginLeft={'5px'}
                           marginRight={'5px'}
                         >
@@ -295,7 +356,7 @@ const AdminLoginPage = () => {
           )}
         </Grid>
         <Card className='footer-card'>
-        <CardContent
+          <CardContent
             sx={{
               pt: '15px !important',
               pb: '15px !important',
@@ -305,13 +366,101 @@ const AdminLoginPage = () => {
             }}
           >
             <Typography
-              sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '1rem' } }}
-              color='black'
+              sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '1rem', fontWeight: '500' } }}
+              color='#ffffff'
               // fontSize={20}
             >
-             © 2024 Adventure Richa Holidays
+              © 2024 Adventure Richa Holidays
             </Typography>
-            {DarkHelpIcon}
+            <Tooltip title='Need Help?' placement='left' arrow>
+              <Box
+                sx={{ backgroundColor: '#ffffff', borderRadius: '50px', display: 'flex', alignItems: 'center' }}
+                onClick={handleClick}
+              >
+                {DarkHelpIcon}
+              </Box>
+            </Tooltip>
+            {/* Popover for the FAQ and Chat options */}
+            <Popover
+              sx={{
+                '& .MuiPopover-paper': {
+                  backgroundColor: 'transparent', // Set background to transparent
+                  boxShadow: 'none', // Remove shadow
+                  padding: '0px', // Optional: remove padding if you want to minimize space
+                  border: 'none', // Optional: Remove the border if you want no outline
+                  left: '1275px !important',
+                  top: '600px !important'
+                }
+              }}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center'
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center'
+              }}
+            >
+              <Box
+                style={{
+                  padding: '10px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'left',
+                  background: '#ffffff00'
+                }}
+              >
+                <Button
+                  sx={{
+                    marginBottom: '10px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    backgroundColor: '#ff7b00',
+                    borderRadius: '10px',
+                    transition: 'all 0.3s ease',
+                    '&:hover': {
+                      backgroundColor: '#ffb76b', // Change the background color on hover
+                      color: 'orange', // Change the text color on hover
+                      transform: 'scale(1.05)', // Optional: add a slight scale effect on hover
+                      boxShadow: 'none' // Optional: add a shadow on hover
+                    }
+                  }}
+                  onClick={handleFaqClick} // Set the onClick handler
+                >
+                  {FAQsIcon}
+                  <Typography sx={{ fontWeight: '600', marginLeft: '10px', color: 'White' }}>FAQs</Typography>
+                </Button>
+
+                <Button
+                  sx={{
+                    marginBottom: '10px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    backgroundColor: '#ff7b00',
+                    borderRadius: '10px',
+                    transition: 'all 0.3s ease',
+                    '&:hover': {
+                      backgroundColor: '#ffb76b', // Change the background color on hover
+                      color: 'orange', // Change the text color on hover
+                      transform: 'scale(1.05)', // Optional: add a slight scale effect on hover
+                      boxShadow: 'none' // Optional: add a shadow on hover
+                    }
+                  }}
+                  onClick={() =>
+                    window.open(
+                      'https://wa.me/+919784189197?text=kindly%20describe%20your%20issue%20before%20starting%20the%20chat%20with%20our%20ARH%20support%20experts.%20Your%20details%20will%20help%20us%20assist%20you%20more%20efficiently!',
+                      '_blank'
+                    )
+                  }
+                >
+                  {HandShake}
+                  <Typography sx={{ fontWeight: '600', marginLeft: '10px', color: 'white' }}>Chat</Typography>
+                </Button>
+              </Box>
+            </Popover>
           </CardContent>
         </Card>
       </Box>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,43 +4,92 @@ import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import Typography from '@mui/material/Typography'
 
 import { useTheme } from '@mui/material/styles'
 
 import BlankLayout from 'src/@core/layouts/BlankLayout'
 
 import LoginRight from 'src/components/login/LoginRight'
-import { DarkHelpIcon } from 'src/utils/icons'
+import { DarkHelpIcon, FAQsIcon, HandShake } from 'src/utils/icons'
 
+import { CardContent, Typography, Tooltip, Popover, Button } from '@mui/material'
 
-const images = ['/images/login_slider1.png', '/images/login_slider2.png', '/images/login_slider3.png']
+import { useRouter } from 'next/router'
 
+const images = ['/images/login_slider1.jpeg', '/images/login_slider2.jpeg', '/images/login_slider3.jpeg']
 
 const LoginPage = () => {
+  const router = useRouter()
   const [currentImage, setCurrentImage] = useState(0)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [open, setOpen] = useState(false)
+  const [background, setBackground] = useState('')
 
   const theme = useTheme()
   const hidden = useMediaQuery(theme.breakpoints.down('md'))
 
+  const getRandomBackground = () => {
+    const randomIndex = Math.floor(Math.random() * urls.length)
+    return urls[randomIndex]
+  }
+
   useEffect(() => {
+    // Set the initial random background
+    setBackground(getRandomBackground())
+
+    // Interval to change the background image (or other behavior) every 5 seconds
     const interval = setInterval(() => {
       setCurrentImage(prevImage => (prevImage + 1) % images.length)
-    }, 5000) // 5000ms = 5 seconds
+    }, 5000)
 
-    return () => clearInterval(interval) // Clean up on unmount
-  }, [])
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval)
+  }, []) // Empty dependency array ensures this runs once on mount
+
+  let urls = [
+    '/images/bakcground-Images/background-arh-indiagate.jpeg',
+    '/images/bakcground-Images/background-arh-tajmaha.jpeg',
+    '/images/bakcground-Images/background-arh-dubai.jpeg',
+    '/images/bakcground-Images/background-arh-dubaiburjkhalifa.jpeg',
+    '/images/bakcground-Images/background-arh-dubaiskyview.jpeg',
+    '/images/bakcground-Images/background-arh-hawamahal.jpeg',
+    '/images/bakcground-Images/background-arh-himachalforest.jpeg',
+    '/images/bakcground-Images/background-arh-himachalmountains.jpeg'
+  ]
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+    setOpen(!open) // Toggle the popover visibility
+  }
+
+  // Handle the close of the Popover
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleFaqClick = () => {
+    // Redirect to the FAQ page
+    router.push('/faqs') // Make sure to replace '/faq' with your actual FAQ route
+  }
 
   return (
-    <Box className='content-right'>
+    <Box
+      sx={{
+        backgroundImage: `url(${background})`,
+        // backgroundImage: `url(${getRandomBackground()})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'space-between',
           width: '100%',
+          height: '100vh',
           px: 10
         }}
       >
@@ -50,18 +99,22 @@ const LoginPage = () => {
         <Grid
           container
           spacing={6}
-          // sx={{
-          //   border: '1px solid red'
-          // }}
+          sx={{
+            // border: '1px solid red',
+            display: 'flex',
+            lineHeight: '0px',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
         >
           {!hidden && (
             <Grid item xs={12} md={6}>
-              <Card sx={{ background: 'none', height: '500px' }}>
+              <Card sx={{ background: 'none', borderRadius: '15px' }}>
                 <img
                   src={images[currentImage]}
                   alt='carousel'
                   width={'100%'}
-                  height={'100%'}
+                  // height={'100%'}
                   style={{ borderRadius: '10px' }}
                 />
               </Card>
@@ -81,7 +134,7 @@ const LoginPage = () => {
           )}
         </Grid>
         <Card className='footer-card'>
-        <CardContent
+          <CardContent
             sx={{
               pt: '15px !important',
               pb: '15px !important',
@@ -91,13 +144,116 @@ const LoginPage = () => {
             }}
           >
             <Typography
-              sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '1rem' } }}
-              color='black'
-              // fontSize={20}
+              sx={{ fontSize: { xs: '0.56rem', sm: '0.75rem', md: '0.56rem', laptopSm: '1rem', fontWeight: '500' } }}
+              color='#ffffff'
+              // fontSize={10}
             >
-             © 2024 Adventure Richa Holidays
+              © 2024 Adventure Richa Holidays
             </Typography>
-            {DarkHelpIcon}
+
+            <Tooltip title='Need Help?' placement='left' arrow>
+              <Box
+                sx={{ backgroundColor: '#ffffff', borderRadius: '50px', display: 'flex', alignItems: 'center' }}
+                onClick={handleClick}
+              >
+                {DarkHelpIcon}
+              </Box>
+            </Tooltip>
+            {/* Popover for the FAQ and Chat options */}
+            <Popover
+              sx={{
+                '& .MuiPopover-paper': {
+                  backgroundColor: 'transparent', // Set background to transparent
+                  boxShadow: 'none', // Remove shadow
+                  padding: '0px', // Optional: remove padding if you want to minimize space
+                  border: 'none', // Optional: Remove the border if you want no outline
+                  left: {
+                    xs: '62% !important', // Center for mobile
+                    sm: '84% !important', // Center for tablet
+                    md: '84% !important', // Position for desktop
+                  },
+                  top: {
+                    xs: '81% !important', // Adjust top position for mobile
+                    sm: '86% !important', // Adjust top position for tablet
+                    md: '86% !important', // Fixed top position for desktop
+                  },
+                  transform: {
+                    xs: 'translateX(-50%)', // Center horizontally for mobile
+                    sm: 'translateX(-50%)', // Center horizontally for tablet
+                    md: 'none', // No transform needed for desktop
+                  },
+                  maxWidth: '100%',
+                }
+              }}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center'
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center'
+              }}
+            >
+              <Box
+                style={{
+                  padding: '10px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'left',
+                  background: '#ffffff00'
+                }}
+              >
+                <Button
+                  sx={{
+                    marginBottom: '10px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    backgroundColor: '#ff7b00',
+                    borderRadius: '10px',
+                    transition: 'all 0.3s ease',
+                    '&:hover': {
+                      backgroundColor: '#ffb76b', // Change the background color on hover
+                      color: 'orange', // Change the text color on hover
+                      transform: 'scale(1.05)', // Optional: add a slight scale effect on hover
+                      boxShadow: 'none' // Optional: add a shadow on hover
+                    }
+                  }}
+                  onClick={handleFaqClick} // Set the onClick handler
+                >
+                  {FAQsIcon}
+                  <Typography sx={{ fontWeight: '600', marginLeft: '10px', color: 'White' }}>FAQs</Typography>
+                </Button>
+
+                <Button
+                  sx={{
+                    marginBottom: '10px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    backgroundColor: '#ff7b00',
+                    borderRadius: '10px',
+                    transition: 'all 0.3s ease',
+                    '&:hover': {
+                      backgroundColor: '#ffb76b', // Change the background color on hover
+                      color: 'orange', // Change the text color on hover
+                      transform: 'scale(1.05)', // Optional: add a slight scale effect on hover
+                      boxShadow: 'none' // Optional: add a shadow on hover
+                    }
+                  }}
+                  onClick={() =>
+                    window.open(
+                      'https://wa.me/+919784189197?text=kindly%20describe%20your%20issue%20before%20starting%20the%20chat%20with%20our%20ARH%20support%20experts.%20Your%20details%20will%20help%20us%20assist%20you%20more%20efficiently!',
+                      '_blank'
+                    )
+                  }
+                >
+                  {HandShake}
+                  <Typography sx={{ fontWeight: '600', marginLeft: '10px', color: 'white' }}>Chat</Typography>
+                </Button>
+              </Box>
+            </Popover>
           </CardContent>
         </Card>
         {/* <Box sx={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', gap: 3, mb: 3 }}>
